5e318147f14dbc96b75447c5f16164dedfe5f1f8
Elliot THIS <minisephirot@hotmail.fr>
2018-11-22 19:24:27 +0100

    Ajout des Test des Cases spéciales

--- enjGame/src/tests/testLauncher.java
+++ enjGame/src/tests/testLauncher.java
@@ -6,6 +6,9 @@ import modele.Hero;
 import modele.LabyGenerator;
 import modele.Labyrinthe;
 import modele.Niveau;
+import modele.elements.Magique;
+import modele.elements.Piege;
+import modele.elements.Teleporteur;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -61,6 +64,34 @@ public class testLauncher extends TestCase {
         }
         assertTrue(hero);
         assertTrue(arrive);
+    }
+
+    public void testTeleporteur(){
+        Hero julien = new Hero();
+        Teleporteur tp1 = new Teleporteur(10, 10, null);
+        Teleporteur tp2 = new Teleporteur(90, 90, tp1);
+        tp1.setTpjumele(tp2);
+        tp1.handleSpecialEffect(julien);
+        assertEquals(julien.getX(), tp2.getX());
+        assertEquals(julien.getY(), tp2.getY());
+    }
+
+    public void testPiege(){
+        Hero julien = new Hero();
+        assertEquals(julien.getPv(),3);
+        Piege piege = new Piege(10, 10);
+        piege.handleSpecialEffect(julien);
+        assertEquals(julien.getPv(),2);
+    }
+
+    public void testMagie(){
+        Hero julien = new Hero();
+        assertEquals(julien.getPv(),3);
+        assertEquals(julien.getPvMax(),3);
+        Magique magique = new Magique(10, 10);
+        magique.handleSpecialEffect(julien);
+        assertEquals(julien.getPv(),4);
+        assertEquals(julien.getPvMax(),4);
     }
 
 }
